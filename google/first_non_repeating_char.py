25c5f68a7ee7b3547ce9df14affa0dc826e3efd5
ibrahim45 <kibrahimsha4@gmail.com>
2020-03-04 00:24:58 +0530

    updated non repeating character

--- google/first_non_repeating_char.py
+++ google/first_non_repeating_char.py
@@ -9,8 +9,10 @@ alphabets_hash = dict((i, index) for index, i in enumerate(string.ascii_letters)
 
 for i in inputstr:
     output[alphabets_hash[i]] += 1
-
+alphabets_keys = list(alphabets_hash.keys())
+print(output)
 for index, i in enumerate(output):
     if i == 1:
         print("first non repeating character is")
-        print(alphabets_hash.keys()[index])
+        print(alphabets_keys[index])
+        break;
